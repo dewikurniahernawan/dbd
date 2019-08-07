d3137a08a96d6effd8acbd52ebe14268446f83ce
Risaaria <marisharisa@gmail.com>
2019-08-07 14:33:09 +0700

    test

--- views/site/about.php
+++ views/site/about.php
@@ -4,7 +4,7 @@
 
 use yii\helpers\Html;
 
-$this->title = 'Abouts';
+$this->title = 'Abouts hskhdsdufldkhfdsugfdfhdfgdsihf';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
