0d4fc6d6092b602419b5da3a0b71016eb1c3db1f
DewiKurnia <dewikurnia7003@gmail.com>
2019-08-07 15:38:42 +0700

    maps

--- views/site/maps.php
+++ views/site/maps.php
@@ -1,17 +1,9 @@
 <?php
 
-
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'Maps';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-maps">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
 </div>
